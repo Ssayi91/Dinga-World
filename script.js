106b7c3956681a93f878bc07bc0e725539d7a913
Sayi <sonny.sayi@gmail.com>
2024-07-22 14:13:41 +0300

    changed styling abit and footer

--- script.js
+++ script.js
@@ -107,3 +107,18 @@ var swiper = new Swiper(".vehicles-slider", {
       },
     },
   });
+  //header
+let header = document.querySelector('header');
+window.addEventListener('scroll', () =>{
+    header.classList.toggle('shadow', window.scrollY > 0)
+});
+// search
+let search   = document.querySelector('.searchbox');
+
+document.querySelector('#search-icon').onclick = ()=> {
+    search.classList.toggle('active');
+}
+document.getElementById('menu-icon').onclick = function() {
+    var navbar = document.querySelector('.navbar');
+    navbar.style.display = navbar.style.display === 'flex' ? 'none' : 'flex';
+}
